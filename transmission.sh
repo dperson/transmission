8b10f669719f06980eb193d62ec79ca45846273d
David Personette <dperson@gmail.com>
2015-02-21 20:27:14 -0500

    transmission.sh set lower uplink limit

--- transmission.sh
+++ transmission.sh
@@ -76,8 +76,11 @@ else
                 gzip -cd > $dir/info/blocklists/bt_level1
     chown debian-transmission. $dir/info/blocklists/bt_level1
     grep -q peer-socket-tos $dir/info/settings.json ||
-        sed -i '/peer-socket-tos/d; /"peer-port"/a \
+        sed -i '/"peer-port"/a \
     "peer-socket-tos": "lowcost",' $dir/info/settings.json
+    sed -i '/"queue-stalled-enabled"/s/:.*/: true,/' $dir/info/settings.json
+    sed -i '/"speed-limit-up"/s/:.*/: 10,/' $dir/info/settings.json
+    sed -i '/"speed-limit-up-enabled"/s/:.*/: true,/' $dir/info/settings.json
     exec su -l debian-transmission -s /bin/bash -c "exec transmission-daemon \
                 --config-dir $dir/info --blocklist --encryption-preferred \
                 --log-error --global-seedratio 2.0 --dht \
