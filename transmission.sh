0c63c95f33f50a5708e59b82fdc55b7518f7ed61
David Personette <dperson@gmail.com>
2016-01-03 08:17:17 -0500

    transmission.sh allow any GID/UID to be set (even dupes)

--- transmission.sh
+++ transmission.sh
@@ -67,7 +67,7 @@ shift $(( OPTIND - 1 ))
 
 [[ "${TZ:-""}" ]] && timezone "$TZ"
 [[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o debian-transmission
-[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o debian-transmission
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]]&& groupmod -g $GROUPID -o debian-transmission
 
 [[ -d $dir/downloads ]] || mkdir -p $dir/downloads
 [[ -d $dir/incomplete ]] || mkdir -p $dir/incomplete
